a847e36987157f1034d048d8ef6ca165bace8211
Kilian "Menkalian <menkalian@web.de>
2019-12-28 03:23:11 +0100

    Jacobi, Main, Testvalues
    - Implementierung des Jacobi-Verfahrens
    - Aufteilung in die Dateien
    - Temporäre Plazierung von Testwerten in der Load-Methode
    - Ausgabe der Vectoren, Abfrage der Eingabeparameter
    - (Bitte den Code noch mal durchsehen. Konstruktive Kritik erwünscht!)
    - CMakeLists für die Dateiaufteilung

--- main.c
+++ main.c
@@ -1,36 +1,139 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include "utils.h"
 
-// Define everything needed
-// Structs
-typedef struct {
-    int n;
-    double **data;
-} Matrix;
-
-typedef struct {
-    int n;
-    double *data;
-} Vector;
-
-// Enum
-typedef enum {
-    JACOBI = 0, GAUSS_SEIDEL = 1
-} Method;
-
-// Methods
-bool load (const char *filename, Matrix *A, Vector *b, Vector *x);
-Vector* solve (Method method, Matrix *A, Vector* b, Vector *x, double e);
+// Solving Methods
+Vector *solve(Method method, Matrix *A, Vector *b, Vector *x, double e);
+Vector *solveJ(Matrix *A, Vector *b, Vector *x, double e, int n);  // JACOBI
+Vector *solveGS(Matrix *A, Vector *b, Vector *x, double e); // GAUSS_SEIDEL
 
 int main() {
-    printf("Hello, World!\n");
+    // Init Variables
+    char *filename = malloc(260 * sizeof(char));    // Maximale Pfadlänge in Windows ist 260.
+    Matrix *A = malloc(sizeof(Matrix));             // A is a pointer to the values of the SLE (System of linear equations)
+    Vector *b = malloc(sizeof(Vector));             // b is a pointer to the right side of the SLE (System of linear equations)
+    Vector *x = malloc(sizeof(Vector));             // x is a pointer to the start values
+    Method method = 0;
+    double margin = -1.0; // Margin of error
+
+    // Read in the values
+    printf("Bitte den Namen der CSV-Datein angeben: ");
+    scanf("%s", filename);
+    while (getchar() != '\n'); // Clear the input buffer
+    bool success = load(filename, A, b, x);
+    printf("%s wurde %serfolgreich eingelesen!\n\n", filename, success ? "" : "nicht ");
+
+    // Abort if not successfull
+    if (!success) {
+        printf("Das Programm wird jetzt beendet!\n");
+        return 1;
+    }
+
+    char c = '0';
+    while (c != 'j' && c != 'J' && c != 'g' && c != 'G') {
+        printf("Soll das Gleichungssystem mit dem (J)acobi-Verfahren oder mit dem (G)auss-Seidel-Verfahren bearbeitet werden? ");
+        scanf("%c", &c);
+        while (getchar() != '\n');
+
+        if (c == 'j' || c == 'J')
+            method = JACOBI;
+        else if (c == 'g' || c == 'G')
+            method = GAUSS_SEIDEL;
+        else
+            printf("Diese Eingabe ist ungültig.\n");
+    }
+
+    while (margin < 0) {
+        printf("\nBei welcher Genauigkeit soll die Berechnung beendet werden? ");
+        scanf("%lf", &margin);
+        while (getchar() != '\n');
+        if (margin < 0)
+            printf("Diese Eingabe ist ungültig! Bitte eine Zahl > 0 eingeben.\n");
+    }
+
+    printf("\nDie Berechnung für die Werte aus \"%s\" wird begonnen. Das Gewählte Verfahren ist das %s-Verfahren. "
+           "Die Berechnung wird bei einem Fehlerwert von weniger als %.10lf beendet.\n",
+           filename, method ? "Gauss-Seidel" : "Jacobi", margin);
+    Vector *result = solve(method, A, b, x, margin);
+    printf("\n***Die Berechnung wurde beendet!***\n\n\n");
+
+    // Ausgabewunsch abfragen
+    printf("Sollen alle Iterationsschritte angezeigt werden? (j/n) ");
+    scanf("%c", &c);
+    while (getchar() != '\n');
+
+    if (c == 'j' || c == 'J') {
+        // Count through the results until we hit the starting Vector to determine the amount of steps
+        int number = 0;
+        while (!vectorCompare(x, &result[number]))
+            number++;
+
+        // PRINT ALL
+        printf("Die Iterationsschritte sind: \n");
+        for (int i = 0; i <= number; ++i) {
+            printf("%d)\t", i);
+            vectorPrint(result[number - i]);
+        }
+    } else {
+        if (c != 'n' && c != 'N')
+            printf("Keine Gültige Angabe. Es wird NEIN als Antwort angenommen.\n");
+
+        // Just print the final result
+        printf("Das Ergebnis ist: \n");
+        vectorPrint(result[0]);
+    }
     return 0;
 }
 
-bool load (const char *filename, Matrix *A, Vector *b, Vector *x){
-    return false;
+Vector *solve(Method method, Matrix *A, Vector *b, Vector *x, double e) {
+    Vector *toRet;
+    if (method == JACOBI) {
+        toRet = solveJ(A, b, x, e, 0);
+    } else { // == GAUSS_SEIDEL
+        printf("GAUSS_SEIDEL noch nicht implementiert!");
+        toRet = solveGS(A, b, x, e);
+    }
+    return toRet;
 }
 
-Vector* solve (Method method, Matrix *A, Vector* b, Vector *x, double e){
+Vector *solveJ(Matrix *A, Vector *b, Vector *x, double e, int n) {
+    if (n >= 100) // End Rekursion
+        return x;
+
+    Vector *toRet = malloc((n + 1) * sizeof(Vector));
+
+    Vector *start = x; // Index 0 is the latest Result, so Array Indexing is not needed
+
+    // Jacobi uses b as base to calculate the next Iteration. So b gets copied to the result first.
+    Vector res;
+    res.data = malloc(b->n * sizeof(double));
+    for (int i = 0; i < b->n; ++i) { // copy the vector. NOT only the pointer
+        res.data[i] = b->data[i];
+    }
+    res.n = b->n;
+    Vector *result = &res;
+
+    // Transform "result" to get the next Vector
+    for (int i = 0; i < A->n; ++i) {
+        for (int j = 0; j < A->n; ++j) {
+            if (j != i) {
+                result->data[i] -= A->data[i][j] * start->data[j];
+            }
+        }
+        result->data[i] /= A->data[i][i];
+    }
+
+    // Build Array toRet
+    toRet[0] = *result;
+    for (int i = 1; i <= n + 1; ++i)
+        toRet[i] = x[i - 1];
+
+    // Continue Calculation or return
+    if (vectorDistance(*start, *result) <= e) {
+        return toRet;
+    } else {
+        return solveJ(A, b, toRet, e, n + 1);
+    }
+}
+
+Vector *solveGS(Matrix *A, Vector *b, Vector *x, double e) {
     return NULL;
 }
---
+++ utils.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+// Define everything needed
+// Structs
+typedef struct {
+    int n;
+    double **data;
+} Matrix;
+
+typedef struct {
+    int n;
+    double *data;
+} Vector;
+
+// Enum
+typedef enum {
+    JACOBI = 0, GAUSS_SEIDEL = 1
+} Method;
+
+bool load(const char *filename, Matrix *A, Vector *b, Vector *x);
+double vectorDistance(Vector v1, Vector v2);
+bool vectorCompare(Vector* v1, Vector* v2);
+void vectorPrint(Vector v);
