53c64c02f9421d259a5037bcb6aa98313514bdbc
Benedikt Hutter <benedikt.hutter5@gmail.com>
2020-01-22 21:22:11 +0100

    Deletion of load Test function and change vector output precision

--- utils.c
+++ utils.c
@@ -1,58 +1,5 @@
 #include "utils.h"
 
-bool loadTest(const char *filename, Matrix *A, Vector *b, Vector *x) {
-    // Fill in Test Values
-    /*
-     * TEST DATA:
-     *  4,2,1,7
-     *  2,4,1,7
-     *  1,1,3,5
-     *
-     *  A = {{4,2,1},{2,4,1},{1,1,3}}
-     *  b = {7,7,5}
-     *  x = {0,0,0}
-     *
-     *  Lösung: {1,1,1}
-     */
-    double **mat = malloc(3 * sizeof(double *));
-    double *r1 = malloc(3 * sizeof(double));
-    double *r2 = malloc(3 * sizeof(double));
-    double *r3 = malloc(3 * sizeof(double));
-    mat[0] = r1;
-    mat[1] = r2;
-    mat[2] = r3;
-
-    double *sol = malloc(3 * sizeof(double));
-    double *start = malloc(3 * sizeof(double));
-
-    // LGS
-    mat[0][0] = 4;
-    mat[0][1] = 2;
-    mat[0][2] = 1;
-    mat[1][0] = 2;
-    mat[1][1] = 4;
-    mat[1][2] = 1;
-    mat[2][0] = 1;
-    mat[2][1] = 1;
-    mat[2][2] = 3;
-
-    // rightside
-    sol[0] = 7.0;
-    sol[1] = 7.0;
-    sol[2] = 5.0;
-
-    // Start
-    start[0] = 0.0;
-    start[1] = 0.0;
-    start[2] = 0.0;
-
-    // Assign the values
-    *A = (Matrix) {.n = 3, .data =  mat};
-    *b = (Vector) {.n = 3, .data = sol};
-    *x = (Vector) {.n = 3, .data = start};
-    return true;
-}
-
 double vectorDistance(Vector v1, Vector v2) {
     if (v1.n != v2.n) {
         printf("Vektoren ungleicher Dimensionen erkannt! Entfernung kann nicht berechnet werden. Breche Berechnung ab!\n");
@@ -84,7 +31,7 @@ bool vectorCompare(Vector* v1, Vector* v2){
 void vectorPrint(Vector v) {
     printf("{");
     for (int i = 0; i < v.n; ++i) {
-        printf(" %.8lf", v.data[i]);
+        printf(" %.10lf", v.data[i]);
         if (i + 1 < v.n )
             printf(",");
     }
