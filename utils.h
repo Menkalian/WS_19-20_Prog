b5d2268dba2e65109e87eb52c70ddfe2374c3ed7
Benedikt Hutter <benedikt.hutter@computer-creativ.com>
2020-03-21 21:43:14 +0100

    Hinzufügen von Funktionsdefinitionen für eine verkettet Liste

--- utils.h
+++ utils.h
@@ -22,7 +22,18 @@ typedef enum {
     GAUSS_SEIDEL = 1
 } Method;
 
+typedef struct ListElement{
+  Vector vector;
+  struct ListElement* next;
+  struct ListElement* last;
+} ListElement;
+
 // Functions
 bool load(const char *filename, Matrix *A, Vector *b, Vector *x);
 double vectorDistance(Vector v1, Vector v2);
 void vectorPrint(Vector v);
+int getListLength(ListElement* startElement);
+void convertListToArray(Vector* vectorArray, ListElement* listStartElement,
+                        int length);
+ListElement* getLastListElement(ListElement* startElement);
+void deleteChainedList(ListElement* startElement);
